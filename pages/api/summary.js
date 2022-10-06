8d590db94b33a6b95f89b353f27517dc2008e1b1
mintck22 <chawanluckkm@gmail.com>
2022-10-06 15:39:19 +0700

    done

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,9 +1,25 @@
+import { checkToken } from "../../backendLibs/checkToken";
+import { readUsersDB } from "../../backendLibs/dbLib";
+
 export default function summaryRoute(req, res) {
   if (req.method === "GET") {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "Permission denied" });
+    const user = checkToken(req);
+    if (!user || !user.isAdmin)
+      return res.status(403).json({
+        ok: false,
+        message: "Permission denied",
+      });
+
     //compute DB summary
+    const users = readUsersDB();
+    const userCount = users.filter((x) => x.isAdmin === false).length;
+    const adminCount = users.filter((x) => x.isAdmin === true).length;
+    const totalMoney = users
+      .filter((x) => x.isAdmin === false)
+      .reduce((p, c) => p.money + c.money, 0);
     //return response
+    return res.json({ ok: true, userCount, adminCount, totalMoney });
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -7,8 +7,14 @@ export default function userRegisterRoute(req, res) {
     const { username, password, isAdmin } = req.body;
 
     //check authentication
-    const user = checkToken(req);
-    //return res.status(403).json({ok: false,message: "You do not have permission to create account",});
+    if (isAdmin) {
+      const user = checkToken(req);
+      if (!user || !user.isAdmin)
+        return res.status(403).json({
+          ok: false,
+          message: "You do not have permission to create account",
+        });
+    }
 
     //validate body
     if (
@@ -24,12 +30,23 @@ export default function userRegisterRoute(req, res) {
 
     //check if username is already in database
     const users = readUsersDB();
-    //return res.status(400).json({ ok: false, message: "Username is already taken" });
+    const foundUser = users.find((x) => x.username === username);
+    if (foundUser)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Username is already taken" });
 
     //create new user and add in db
-
+    const newUser = {
+      username,
+      password: bcrypt.hashSync(password, 12),
+      isAdmin,
+      money: isAdmin ? null : 0,
+    };
+    users.push(newUser);
     writeUsersDB(users);
 
     //return response
+    return res.json({ ok: true, username, isAdmin });
   }
 }
